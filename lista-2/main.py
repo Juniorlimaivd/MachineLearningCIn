2b8b1dbe8a3021a363b6152947ef19cc286b6c35
Unknown <junior.lima.ivd.2@gmail.com>
2018-04-13 11:12:33 -0300

    Finish generating plots

--- lista-2/main.py
+++ lista-2/main.py
@@ -31,14 +31,117 @@ def normalize(dataFrame):
     dataFrame[y_collumn_name] = y.values
     return dataFrame
 
-if __name__ == "__main__":
-    dataFrame = loadDataset(sys.argv[1], sys.argv[2])
-    dataFrame = normalize(dataFrame)
-    
-    trainingSet, testSet = train_test_split(dataFrame, test_size=0.33, stratify=dataFrame.iloc[:,-1])
+def testKnnLvqComparison(trainingSet, testSet, knn_sizes):
 
-    knn_sizes = [1,3]
-    prototypes_sizes = [10,50,100,200,300]
+    accuracys_lvq = []
+    accuracys_knn = []
+
+    times_training_lvq = []
+    times_training_knn = []
+    times_test_lvq = []
+    times_test_knn = []
+    times_total_lvq = []
+    times_total_knn = []
+
+
+    for size in knn_sizes:
+        # -------------------------------- START LVQ TEST ----------------------------------
+        t0_total_lvq = timer()
+
+        t0_training_lvq = timer()
+        prot, classes = lvq1(trainingSet, prototypesNumber=100)
+        t1_training_lvq = timer()
+        times_training_lvq.append(t1_training_lvq - t0_training_lvq)
+
+        hits = 0
+        classifier = knn(n_neighbors=size)
+        
+        classifier.fit(prot,classes)
+
+        x = testSet.iloc[:, :-1].values
+        y = testSet.iloc[:, -1].values
+        t0_teste_lvq = timer()
+        output = classifier.predict(x)
+        t1_teste_lvq = timer()
+        times_test_lvq.append(t1_teste_lvq - t0_teste_lvq)
+
+        for i in range(len(output)):
+            if output[i]  == y[i]:
+                hits += 1
+
+        accuracys_lvq.append(100.0*hits/float(len(testSet.values)))
+        t1_total_lvq = timer()
+        times_total_lvq.append(t1_total_lvq - t0_total_lvq)
+        
+        # -------------------------------- START KNN TEST ----------------------------------
+
+        t0_total_knn = timer()
+        hits = 0
+        classifier = knn(n_neighbors=size)
+
+        x = trainingSet.iloc[:, :-1].values
+        y = trainingSet.iloc[:, -1].values
+
+        t0_training_knn = timer()
+        classifier.fit(x,y)
+        t1_training_knn = timer()
+        times_training_knn.append(t1_training_knn - t0_training_knn)
+
+        x = testSet.iloc[:, :-1].values
+        y = testSet.iloc[:, -1].values
+        t0_teste_knn = timer()
+        output = classifier.predict(x)
+        t1_teste_knn = timer()
+
+        times_test_knn.append(t1_teste_knn - t0_teste_knn)
+
+        for i in range(len(output)):
+            if output[i]  == y[i]:
+                hits += 1
+        
+        accuracys_knn.append(100.0*hits/float(len(testSet.values)))
+
+        t1_total_knn = timer()
+
+        times_total_knn.append(t1_total_knn - t0_total_knn)
+
+    width = 0.35
+
+    # plt.title("knn x lvq com 100 prototipos por taxa de acerto")
+    # rect1 = plt.bar(knn_sizes,accuracys_lvq, color='#d62728', width=width)
+    # rect2 = plt.bar(map(lambda x : x + width, knn_sizes),accuracys_knn, width=width)
+    # plt.legend( (rect1[0], rect2[0]), ('LVQ+KNN', 'KNN') )
+    # plt.xticks(knn_sizes,('1', '3'))
+    # plt.xlabel("Numero de vizinhos")
+    # plt.ylabel("Taxa de acerto")
+    # plt.ylim(0,100)
+    # plt.savefig("lvq3_knn_accuracy"+".png")
+    # plt.clf()
+
+    plt.title("knn x lvq com 100 prototipos por tempo de processamento")
+    rect1 = plt.bar(knn_sizes,times_test_lvq, color='#d62728', width=width)
+    rect2 = plt.bar(map(lambda x : x + width, knn_sizes),times_test_knn, width=width)
+    plt.legend( (rect1[0], rect2[0]), ('LVQ+KNN', 'KNN') )
+    plt.xticks(knn_sizes,('1', '3'))
+    plt.xlabel("Numero de vizinhos")
+    plt.ylabel("Tempo de processamento(s)")
+    plt.ylim(0,1)
+    plt.savefig("lvq1_knn_time_teste"+".png")
+    plt.clf() 
+    
+    # plt.title("knn x lvq com 100 prototipos por tempo de processamento")
+    # rect1 = plt.bar(knn_sizes,times_total_lvq, color='#d62728', width=width)
+    # rect2 = plt.bar(map(lambda x : x + width, knn_sizes),times_total_knn, width=width)
+    # plt.legend( (rect1[0], rect2[0]), ('LVQ+KNN', 'KNN') )
+    # plt.xticks(knn_sizes,('1', '3'))
+    # plt.xlabel("Numero de vizinhos")
+    # plt.ylabel("Tempo de processamento(s)")
+    # # plt.ylim(0,1)
+    # plt.savefig("lvq3_knn_time_total"+".png")
+    # plt.clf()
+
+def runLVQTimeAccuracyTests(trainingSet, testSet, knn_sizes, prototypes_sizes):
+
     times_prototype_gen = []
     
 
@@ -48,7 +151,7 @@ if __name__ == "__main__":
         for n_prototypes in prototypes_sizes: 
             acc_array = []
             times_prototype_gen = []
-            # for _ in range(10):
+            
             t0_gen = timer()
             prot, classes = lvq1(trainingSet, prototypesNumber=n_prototypes)
             t1_gen = timer()
@@ -73,117 +176,34 @@ if __name__ == "__main__":
             times_protot.append(reduce(lambda x, y: x + y, times_prototype_gen) / len(times_prototype_gen))
             accuracy.append(reduce(lambda x, y: x + y, acc_array) / len(acc_array))
         # print(accuracy)
-        plt.title("prototipos_x_acerto_knn_"+str(size))
-        plt.bar(prototypes_sizes, accuracy, width=25)
-        plt.xlabel("Numero de prototipos")
-        plt.ylabel("Taxa de acerto")
-        plt.ylim(0,50)
-        plt.savefig("prototipos_x_acerto_knn_"+str(size)+".png")
-        plt.clf()
+        # plt.title("prototipos_x_acerto_knn_"+str(size))
+        # plt.bar(prototypes_sizes, accuracy, width=25)
+        # plt.xlabel("Numero de prototipos")
+        # plt.ylabel("Taxa de acerto")
+        # plt.ylim(75,100)
+        # plt.savefig("prototipos_x_acerto_knn_"+str(size)+".png")
+        # plt.clf()
 
-    # plt.title("tempo de processamento para geracao dos prototipos")
-    # plt.bar(prototypes_sizes,times_protot, width=35)
-    # plt.xlabel("Numero de prototipos")
-    # plt.ylabel("Tempo de processamento(s)")
-    # plt.savefig("lvq3_protot_time"+".png")
-    # plt.clf()
+    plt.title("tempo de processamento para geracao dos prototipos")
+    plt.bar(prototypes_sizes,times_protot, width=35)
+    plt.xlabel("Numero de prototipos")
+    plt.ylabel("Tempo de processamento(s)")
+    plt.savefig("lvq1_protot_time"+".png")
+    plt.clf()
 
-    # accuracys_lvq = []
-    # accuracys_knn = []
-
-    # times_training_lvq = []
-    # times_training_knn = []
-    # times_test_lvq = []
-    # times_test_knn = []
-    # times_total_lvq = []
-    # times_total_knn = []
-
-
-    # for size in knn_sizes:
-    #     t0_total_lvq = timer()
-
-    #     t0_training_lvq = timer()
-    #     prot, classes = lvq1(trainingSet, prototypesNumber=100)
-    #     t1_training_lvq = timer()
-    #     times_training_lvq.append(t1_training_lvq - t0_training_lvq)
-
-    #     hits = 0
-    #     classifier = knn(n_neighbors=size)
-        
-    #     classifier.fit(prot,classes)
-
-    #     x = testSet.iloc[:, :-1].values
-    #     y = testSet.iloc[:, -1].values
-    #     t0_teste_lvq = timer()
-    #     output = classifier.predict(x)
-    #     t1_teste_lvq = timer()
-    #     times_test_lvq.append(t1_teste_lvq - t0_teste_lvq)
-
-    #     for i in range(len(output)):
-    #         if output[i]  == y[i]:
-    #             hits += 1
-
-    #     accuracys_lvq.append(100.0*hits/float(len(testSet.values)))
-    #     t1_total_lvq = timer()
-    #     times_total_lvq.append(t1_total_lvq - t0_total_lvq)
-
-
-    #     t0_total_knn = timer()
-    #     hits = 0
-    #     classifier = knn(n_neighbors=size)
-
-    #     x = trainingSet.iloc[:, :-1].values
-    #     y = trainingSet.iloc[:, -1].values
-
-    #     t0_training_knn = timer()
-    #     classifier.fit(x,y)
-    #     t1_training_knn = timer()
-    #     times_training_knn.append(t1_training_knn - t0_training_knn)
-
-    #     x = testSet.iloc[:, :-1].values
-    #     y = testSet.iloc[:, -1].values
-    #     t0_teste_knn = timer()
-    #     output = classifier.predict(x)
-    #     t1_teste_knn = timer()
-
-    #     times_test_knn.append(t1_teste_knn - t0_teste_knn)
-
-    #     for i in range(len(output)):
-    #         if output[i]  == y[i]:
-    #             hits += 1
-        
-    #     accuracys_knn.append(100.0*hits/float(len(testSet.values)))
-
-    #     t1_total_knn = timer()
-
-    #     times_total_knn.append(t1_total_knn - t0_total_knn)
-
+if __name__ == "__main__":
+    dataFrame = loadDataset(sys.argv[1], sys.argv[2])
+    dataFrame = normalize(dataFrame)
     
-    # width = 0.35
+    trainingSet, testSet = train_test_split(dataFrame, test_size=0.33, stratify=dataFrame.iloc[:,-1])
+    knn_sizes = [1,3]
+    prototypes_sizes = [10,50,100,200,300]
 
-    # plt.title("knn x lvq com 100 prototipos por taxa de acerto")
-    # rect1 = plt.bar(knn_sizes,accuracys_lvq, color='#d62728', width=width)
-    # rect2 = plt.bar(map(lambda x : x + width, knn_sizes),accuracys_knn, width=width)
-    # plt.legend( (rect1[0], rect2[0]), ('LVQ+KNN', 'KNN') )
-    # plt.xticks(knn_sizes,('1', '3'))
-    # plt.xlabel("Numero de vizinhos")
-    # plt.ylabel("Taxa de acerto")
-    # plt.ylim(0,100)
-    # plt.savefig("lvq3_knn_accuracy"+".png")
-    # plt.clf()
+    #runLVQTimeAccuracyTests(trainingSet, testSet, knn_sizes,prototypes_sizes)
 
-    # width = 0.35
+    testKnnLvqComparison(trainingSet,testSet,knn_sizes)
+    
 
-    # plt.title("knn x lvq com 100 prototipos por tempo de processamento")
-    # rect1 = plt.bar(knn_sizes,times_test_lvq, color='#d62728', width=width)
-    # rect2 = plt.bar(map(lambda x : x + width, knn_sizes),times_test_knn, width=width)
-    # plt.legend( (rect1[0], rect2[0]), ('LVQ+KNN', 'KNN') )
-    # plt.xticks(knn_sizes,('1', '3'))
-    # plt.xlabel("Numero de vizinhos")
-    # plt.ylabel("Tempo de processamento(s)")
-    # plt.ylim(0,0.0030)
-    # plt.savefig("lvq1_knn_time_teste"+".png")
-    # plt.clf()
 
 
 
